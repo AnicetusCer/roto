a093843bd35ea6c142993cd6b2f4c30f89a9c314
AnicetusCer <anicetus_cer@hotmail.com>
2025-11-27 19:37:25 +0000

    Bump to 1.0.2 and update metadata

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "org.roto"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
     }
 
     compileOptions {
